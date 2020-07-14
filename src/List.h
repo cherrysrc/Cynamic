babfc40cd630611dd8b12b912bfb36d519ec0b67
Jerrit Gläsker <50867655+cherrysrc@users.noreply.github.com>
2020-07-14 17:46:07 +0200

    Added at function and Documentation

--- src/List.h
+++ src/List.h
@@ -1,64 +1,94 @@
+/**
+ * @author Jerrit Gläsker
+ * @copyright MIT-License
+ *
+ * @version 14 07 2020
+ * @brief Dynamic Array implementation
+ */
+
 #ifndef PROJECTION_LIST_H
 #define PROJECTION_LIST_H
 
 #include <stdlib.h>
 
 /**
- * List type definition
+ * List structure
+ * @param  T type of list data
  */
-#define List(T) \
-    struct{\
-        unsigned int capacity;\
-        unsigned int count;\
-        T* data;\
+#define List(T)                \
+    struct {                   \
+        unsigned int capacity; \
+        unsigned int count;    \
+        T*           data;     \
     }
 
 /**
- * Creates a new list of a given type and initial capacity
+ * Creates a new list of the given type and capacity
+ * @param  T        Type
+ * @param  capacity Capacity
+ * @return          new list
  */
-#define newList(T, capacity) \
-    {\
-        capacity,\
-        0,\
-        (T*)calloc(capacity, sizeof(T))\
+#define newList(T, capacity)            \
+    {                                   \
+        capacity,                       \
+        0,                              \
+        (T*)calloc(capacity, sizeof(T)) \
     }
 
 /**
- * Appends an element to the given list
+ * Append an item to the given list
+ * @param  list list to operate on
+ * @param  elt  element to append
  */
-#define appendList(list, elt)\
-    if(list.count >= list.capacity){\
-        list.capacity*=2;\
-        list.data = (typeof(list.data))realloc(list.data, list.capacity * sizeof(*list.data));\
-    }\
+#define appendList(list, elt)                                                                       \
+    if (list.count >= list.capacity) {                                                              \
+        list.capacity *= 2;                                                                         \
+        list.data      = (typeof(list.data))realloc(list.data, list.capacity * sizeof(*list.data)); \
+    }                                                                                               \
     list.data[list.count++] = elt
 
 /**
- * Prepends an element to the given list
+ * Prepent item to list
+ * @param  list list to operate on
+ * @param  elt  element to prepend
  */
-#define prependList(list, elt)\
-    appendList(list, elt);\
-    for(int i = list.count - 1; i >= 1; i--){\
-        list.data[i] = list.data[i-1];\
-    }\
+#define prependList(list, elt)                  \
+    appendList(list, elt);                      \
+    for (int i = list.count - 1; i >= 1; i--) { \
+        list.data[i] = list.data[i - 1];        \
+    }                                           \
     list.data[0] = elt
 
 /**
- * Removes and returns the last element of the given list
+ * Removes and returns the last element
+ * @param  list list to operate on
+ * @return      last item of list
  */
-#define popList(list)\
+#define popList(list) \
     list.data[list.count--]
 
 /**
- * Sorts a given list
+ * Returns item at position
+ * @param  list list to retrieve from
+ * @param  i    index to retrieve from
+ * @return      item at given location
  */
-#define sortList(list, compareFunction)\
+#define at(list, i) \
+    list.data[i];
+
+/**
+ * Sorts a list using the builtin qsort function
+ * @param  list            list to sort
+ * @param  compareFunction function for comparing two elements
+ */
+#define sortList(list, compareFunction) \
     qsort(list.data, list.count, sizeof(*list.data), compareFunction)
 
 /**
- * Clean memory used by list
+ * Frees memory used by a given list
+ * @param  list list to operate on
  */
-#define cleanupList(list)\
+#define cleanupList(list) \
     free(list.data)
 
 #endif //PROJECTION_LIST_H
